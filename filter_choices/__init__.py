0677cf8f895ef4aa97a0e34d8827b62793406e26
darnell-love27 <101828196+darnell-love27@users.noreply.github.com>
2023-05-06 15:18:25 -0400

    Initial commit

---
+++ filter_choices/__init__.py
@@ -0,0 +1,16 @@
+from .Filter_Choices import Filters
+
+__author__ = 'Miles Adedjouma, Jamar Bailey III, Darnell Love'
+__email__ = 'N/A'
+__version__ = '1.0.0'
+
+# module level doc-string
+__doc__ = """
+Filter Choices
+================
+
+Description
+-----------
+The project was meant to be a test of the package functions in Github. In order to so, we created a project that was a photo filter function package using Python. The goal was to allow the user to take the package in order to add it to a greater function or to modify the function to allow users to add their own filters to the image. Furthermore, the function would allow users to input their own images in the functions. 
+
+"""
